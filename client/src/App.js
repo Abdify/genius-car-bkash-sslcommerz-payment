ea3b3aba2dbb36a9c1743db503bce3c8165148b7
Abdify <abdullah.fahimm08@gmail.com>
2022-11-22 13:00:20 +0600

    jsconfig updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from "react-toastify";
 import './App.css';
 import router from './Router/Routes/Routes';
+
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 
   return (
     <div className='max-w-screen-xl mx-auto'>
       <RouterProvider router={router}></RouterProvider>
+      <ToastContainer />
     </div>
   );
 }
