f31ff6d37d4231500d7a6a861d2534578bbf415f
Abdify <abdullah.fahimm08@gmail.com>
2022-11-26 09:14:11 +0600

    payment initialized

--- client/src/Pages/Checkout/Checkout.js
+++ client/src/Pages/Checkout/Checkout.js
@@ -1,75 +1,122 @@
-import React, { useContext } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import { useContext } from "react";
+import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 
 const Checkout = () => {
-    const { _id, title, price } = useLoaderData();
-    const { user } = useContext(AuthContext);
+  const { _id, title, price, img } = useLoaderData();
+  const { user } = useContext(AuthContext);
 
-    const handlePlaceOrder = event => {
-        event.preventDefault();
-        const form = event.target;
-        const name = `${form.firstName.value} ${form.lastName.value}`;
-        const email = user?.email || 'unregistered';
-        const phone = form.phone.value;
-        const message = form.message.value;
+  const handlePlaceOrder = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = `${form.firstName.value} ${form.lastName.value}`;
+    const email = user?.email || "unregistered";
+    const phone = form.phone.value;
+    const address = form.address.value;
+    const postcode = form.postcode.value;
+    const currency = form.currency.value;
 
-        const order = {
-            service: _id,
-            serviceName: title,
-            price,
-            customer: name,
-            email,
-            phone,
-            message
-        }
+    const order = {
+      service: _id,
+      serviceName: title,
+      price,
+      customer: name,
+      email,
+      phone,
+      address,
+      postcode,
+      currency
+    };
 
-        // if(phone.length > 10){
-        //     alert('Phone number should be 10 characters or longer')
-        // }
-        // else{
+    // if(phone.length > 10){
+    //     alert('Phone number should be 10 characters or longer')
+    // }
+    // else{
 
-        // }
+    // }
 
-        fetch('http://localhost:5000/orders', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-                authorization: `Bearer ${localStorage.getItem('genius-token')}`
-            },
-            body: JSON.stringify(order)
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                if(data.acknowledged){
-                    alert('Order placed successfully')
-                    form.reset();
-                    
-                }
-            })
-            .catch(er => console.error(er));
+    fetch("http://localhost:5000/orders", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("genius-token")}`,
+      },
+      body: JSON.stringify(order),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        window.location.replace(data.url);
+      })
+      .catch((er) => console.error(er));
+  };
 
-
-    }
-
-    return (
+  return (
+    <div>
+      <form onSubmit={handlePlaceOrder} className="flex items-center justify-between">
         <div>
-            <form onSubmit={handlePlaceOrder}>
-                <h2 className="text-4xl">You are about to order: {title}</h2>
-                <h4 className="text-3xl">Price: {price}</h4>
-                <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
-                    <input name="firstName" type="text" placeholder="First Name" className="input input-ghost w-full  input-bordered" />
-                    <input name="lastName" type="text" placeholder="Last Name" className="input input-ghost w-full  input-bordered" />
-                    <input name="phone" type="text" placeholder="Your Phone" className="input input-ghost w-full  input-bordered" required />
-                    <input name="email" type="text" placeholder="Your email" defaultValue={user?.email} className="input input-ghost w-full  input-bordered" readOnly />
-                </div>
-                <textarea name="message" className="textarea textarea-bordered h-24 w-full" placeholder="Your Message" required></textarea>
-
-                <input className='btn' type="submit" value="Place Your Order" />
-            </form>
+          <h2 className="text-4xl">You are about to order: {title}</h2>
+          <h4 className="text-3xl">Price: {price}</h4>
+          <img src={img} alt="" />
         </div>
-    );
+        <div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            <input
+              name="firstName"
+              type="text"
+              placeholder="First Name"
+              className="input input-ghost w-full  input-bordered"
+            />
+            <input
+              name="lastName"
+              type="text"
+              placeholder="Last Name"
+              className="input input-ghost w-full  input-bordered"
+            />
+            <input
+              name="phone"
+              type="text"
+              placeholder="Your Phone"
+              className="input input-ghost w-full  input-bordered"
+              required
+            />
+            <input
+              name="email"
+              type="text"
+              placeholder="Your email"
+              defaultValue={user?.email}
+              className="input input-ghost w-full  input-bordered"
+              readOnly
+            />
+          <select
+            defaultValue="BDT"
+            name="currency"
+            className="select select-bordered max-w-xs"
+          >
+            <option value="BDT">BDT</option>
+            <option value="USD">USD</option>
+          </select>
+
+          <input
+            type="text"
+            name="postcode"
+            placeholder="Your Postcode"
+            className="input input-ghost w-full  input-bordered"
+          />
+          </div>
+
+
+          <textarea
+            name="address"
+            className="textarea textarea-bordered h-24 w-full my-5"
+            placeholder="Your Address"
+            required
+          ></textarea>
+
+          <input className="btn w-full" type="submit" value="Pay" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Checkout;
--- server/index.js
+++ server/index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const SSLCommerzPayment = require("sslcommerz-lts");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASSWORD;
+const is_live = false; //true for live, false for sandbox
 
 // middle wares
 app.use(cors());
@@ -101,8 +104,49 @@ async function run() {
 
         app.post("/orders", verifyJWT, async (req, res) => {
             const order = req.body;
-            const result = await orderCollection.insertOne(order);
-            res.send(result);
+
+            const orderedService = await serviceCollection.findOne({ _id: ObjectId(order.service)});
+
+            console.log(orderedService);
+            
+            const data = {
+              total_amount: orderedService.price,
+              currency: order.currency,
+              tran_id: new ObjectId().toString(), // use unique tran_id for each api call
+              success_url: "http://localhost:3030/success",
+              fail_url: "http://localhost:3030/fail",
+              cancel_url: "http://localhost:3030/cancel",
+              ipn_url: "http://localhost:3030/ipn",
+              shipping_method: "Courier",
+              product_name: "Computer.",
+              product_category: "Electronic",
+              product_profile: "general",
+              cus_name: order.customer,
+              cus_email: order.email,
+              cus_add1: order.address,
+              cus_add2: "Dhaka",
+              cus_city: "Dhaka",
+              cus_state: "Dhaka",
+              cus_postcode: "1000",
+              cus_country: "Bangladesh",
+              cus_phone: "01711111111",
+              cus_fax: "01711111111",
+              ship_name: "Customer Name",
+              ship_add1: "Dhaka",
+              ship_add2: "Dhaka",
+              ship_city: "Dhaka",
+              ship_state: "Dhaka",
+              ship_postcode: order.postcode,
+              ship_country: "Bangladesh",
+            };
+
+            const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+            sslcz.init(data).then((apiResponse) => {
+              // Redirect the user to payment gateway
+              let GatewayPageURL = apiResponse.GatewayPageURL;
+              console.log(apiResponse);
+              res.send({url: GatewayPageURL});
+            });
         });
 
         app.patch("/orders/:id", verifyJWT, async (req, res) => {
